8e6dd762d3b2de65a140d749ebee43a0d2a3c802
leschultz <laneenriqueschultz@gmail.com>
2019-06-18 09:43:11 -0500

    Checked the mean as a function of runs included .

--- relaxation_steps/mean_ico_population_dependence.py
+++ relaxation_steps/mean_ico_population_dependence.py
@@ -7,6 +7,7 @@ from scipy import stats
 import pandas as pd
 import numpy as np
 
+import copy
 import sys
 import os
 
@@ -15,7 +16,9 @@ dataname = 'ico_fraction_vs_temperature.txt'
 datapath = sys.argv[1]  # The path containing ICO fractions for all runs
 exportpath = sys.argv[2]  # The path to export analysis for mean
 
-count = 0
+fig, ax = pl.subplots()
+
+i = 1
 for path, subdirs, files in os.walk(datapath):
 
     if dataname not in files:
@@ -23,69 +26,52 @@ for path, subdirs, files in os.walk(datapath):
 
     run = path.strip(datapath)
 
-    if count == 0:
+    if i == 1:
         df = pd.read_csv(join(path, dataname))
-        cols = list(df.columns)
-        col = cols[-1]+'_'
-        cols[-1] = col+str(count)
-        df.columns = cols
 
     else:
-        df[col+str(count)] = pd.read_csv(
-                                         join(path, dataname)
-                                         )['fracs']
+        df = df.merge(
+                      pd.read_csv(join(path, dataname)),
+                      on=['temp'],
+                      suffixes=(i-1, i)
+                      )
 
-    count += 1
+        fracs = df.loc[:, df.columns != 'temp']
 
-means = []
-stds = []
-sems = []
-counts = []
+        temps = df['temp']
+        mean = fracs.mean(axis=1)
+        std = fracs.std(axis=1)
+        sem = fracs.sem(axis=1)
+        count = fracs.count(axis=1)
 
-count = 0
-data = df.values.T
-for i in data:
+        ax.errorbar(
+                    count,
+                    mean,
+                    std,
+                    marker='.', 
+                    ecolor='r',
+                    linestyle='none',
+                    )
 
-    if count > 1:
-        mean = np.mean(data[1:count+1], axis=0)
-        std = np.std(data[1:count+1], axis=0)
-        sem = stats.sem(data[1:count+1], ddof=1, axis=0)
+        ax.errorbar(
+                    count,
+                    mean,
+                    sem,
+                    marker='.',
+                    ecolor='y',
+                    linestyle='none',
+                    )
 
-        means.append(mean)
-        stds.append(std)
-        sems.append(sem)
-        counts.append(count)
+    i += 1
 
-    count += 1
-
-fig, ax = pl.subplots()
-
-for mean, std, sem, count in zip(means, stds, sems, counts):
-
-    ax.errorbar(
-                df['temp'],
-                mean,
-                std,
-                marker='.', 
-                ecolor='r',
-                linestyle='none',
-                label='std'
-                )
-
-    ax.errorbar(
-                df['temp'],
-                mean,
-                sem,
-                marker='.',
-                ecolor='y',
-                linestyle='none',
-                label='sem'
-                )
-
-ax.set_xlabel('Temperature [K]')
+ax.set_xlabel('Count of Runs [-]')
 ax.set_ylabel('Mean ICO Fractions [-]')
-
-ax.legend()
 ax.grid()
 
-pl.show()
+fig.tight_layout()
+
+export = join(exportpath, 'ico')
+if not os.path.exists(export):
+    os.makedirs(export)
+
+fig.savefig(join(export, 'mean_ico_vs_population'))
